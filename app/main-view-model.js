81bd02f4023f0b97cb0b876238a022e8454c530a
harshavamsi <harshavamsi096@gmail.com>
2017-10-07 16:19:46 +0530

    First

---
+++ app/main-view-model.js
@@ -0,0 +1,35 @@
+var Observable = require("data/observable").Observable;
+var firebase = require("nativescript-plugin-firebase");
+function getMessage(counter) {
+    if (counter <= 0) {
+        return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
+    } else {
+        return counter + " taps left";
+    }
+}
+
+function createViewModel() {
+    var viewModel = new Observable();
+    firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+}).then(
+    function (instance) {
+      console.log("firebase.init done");
+    },
+    function (error) {
+      console.log("firebase.init error: " + error);
+    }
+);
+    viewModel.counter = 42;
+    viewModel.message = getMessage(viewModel.counter);
+
+    viewModel.onTap = function() {
+        this.counter--;
+        this.set("message", getMessage(this.counter));
+    }
+
+    return viewModel;
+}
+
+exports.createViewModel = createViewModel;
